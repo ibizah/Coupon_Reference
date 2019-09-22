0eb2d87479d2f2da36ca749928bac5984a77399d
Ibrahim <ibipriya@gmail.com>
2019-09-22 13:09:58 +0530

    validation added

--- Coupon_app2/forms.py
+++ Coupon_app2/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from . models import User
+from . models import  User
 from django.forms import ModelForm
+from django.core import validators
 
 class FormName(forms.Form):
     name= forms.CharField(label='Full Name :')
@@ -8,7 +9,13 @@ class FormName(forms.Form):
     text= forms.CharField(widget=forms.Textarea)
 
 class UserForm(forms.ModelForm):
+    alphanumeric =validators.RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+    name= forms.CharField()
+    email= forms.EmailField()
+    age= forms.IntegerField()
+    city= forms.CharField()
+    referal_code = forms.CharField(required=False, validators=[alphanumeric, validators.MaxLengthValidator(7)])
+
     class Meta:
         model= User
         fields= '__all__'
- 
--- Coupon_app2/models.py
+++ Coupon_app2/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.forms import ModelForm
-from django.core import validators
+
 import string, random
 
 
 # Create your models here.
 
 class User(models.Model):
-    alphanumeric =validators.RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+
     name= models.CharField(max_length=200)
     email= models.EmailField(max_length=100)
     age= models.IntegerField()
     city= models.CharField(max_length=100)
-    referal_code = models.CharField(max_length=7, blank=True, null=True, unique=True, validators=[alphanumeric])
+    referal_code = models.CharField(max_length=7, blank=True, null=True, unique=True,)
 
 
     def generate_verification_code(self):
@@ -24,8 +24,8 @@ class User(models.Model):
 
 
     def save(self, *args, **kwargs):
-        if not self.pk:
-            self.referal_code = self.generate_verification_code()
+        #if not self.pk:
+            #self.referal_code = self.generate_verification_code()
         if not self.referal_code:
             self.referal_code = self.generate_verification_code()
         return super(User, self).save(*args, **kwargs)
